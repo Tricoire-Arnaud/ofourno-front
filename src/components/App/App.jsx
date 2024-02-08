110a80018b58c7ce8213fc38481602bc04b2a0ec
Fonsalexandre <Alexandre.fons@hotmail.fr>
2024-02-08 16:10:46 +0100

    integration Herosection

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Header from '../Header/Header';
-import './App.scss';
 import SearchBar from '../SearchBar/SearchBar';
+import HeroSection from '../Herosection/HeroSection';
+import '../../styles/_variables.scss';
+
+import './App.scss';
 
 function App() {
   return (
     <div className="home-phone">
       <Header />
       <SearchBar />
+      <HeroSection />
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import SearchBar from '../SearchBar/SearchBar';
 import list from '../../assets/list.svg';
 import Logo from '../../assets/logo.svg';
-import onnexion from '../../assets/connexion.svg';
+import Connexion from '../../assets/connexion.svg';
 
 import './Header.scss';
 
@@ -17,3 +16,12 @@ function Header() {
 }
 
 export default Header;
+
+<div className="Header" style={{width: '100%', height: '100%', paddingLeft: 42, paddingRight: 42, justifyContent: 'space-between', alignItems: 'center', display: 'inline-flex'}}>
+    <div className="List" style={{height: 37, paddingLeft: 10, paddingRight: 10, paddingTop: 12, paddingBottom: 12, background: '#9CB7C9', borderRadius: 10, overflow: 'hidden', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', display: 'inline-flex'}}>
+        <div className="Vector" style={{width: 16.45, height: 13.45, background: 'white', borderRadius: 50}}></div>
+    </div>
+    <div className="ConnexionButton" style={{width: 37, height: 37, padding: 6, background: '#9CB7C9', borderRadius: 10, overflow: 'hidden', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', display: 'inline-flex'}}>
+        <div className="Vector" style={{alignSelf: 'stretch', flex: '1 1 0', background: 'white'}}></div>
+    </div>
+</div>
