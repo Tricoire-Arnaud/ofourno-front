b5b2d69645996b571658df7ef2bf743114ed5b8e
Jason Pretavoine <128550353+jasonpretavoine@users.noreply.github.com>
2024-03-08 17:48:51 +0100

    Sécurisation du formulaire

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -17,10 +17,84 @@ function Registration() {
   const [username, setUsername] = useState('');
   const [pseudo, setPseudo] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordStrength, setPasswordStrength] = useState({
+    score: 0,
+    label: 'Vide',
+  });
+  // Evaluation de la force du mot de passe
+  const evaluatePasswordStrength = () => {
+    let strength = 0;
 
+    if (!password) {
+      return { score: 0, label: 'Vide' };
+    }
+
+    // Critères d'évaluation
+    const lengthCriteria = password.length > 8;
+    const digitCriteria = /\d/.test(password);
+    const specialCharCriteria = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    const uppercaseCriteria = /[A-Z]/.test(password);
+    const lowercaseCriteria = /[a-z]/.test(password);
+
+    // Calcul du score
+    strength += lengthCriteria ? 1 : 0;
+    strength += digitCriteria ? 1 : 0;
+    strength += specialCharCriteria ? 1 : 0;
+    strength += uppercaseCriteria ? 1 : 0;
+    strength += lowercaseCriteria ? 1 : 0;
+
+    // Attribution de l'étiquette en fonction du score
+    let label = 'Faible';
+    if (strength >= 4) {
+      label = 'Fort';
+    } else if (strength === 3) {
+      label = 'Moyen';
+    }
+
+    return { score: strength, label };
+  };
+
+  const handlePasswordChange = (e) => {
+    const newPassword = e.target.value;
+    setPassword(newPassword); // Met à jour l'état du mot de passe
+    const strength = evaluatePasswordStrength(newPassword); // Évalue la force du mot de passe
+    setPasswordStrength(strength); // Met à jour l'état de la force du mot de passe
+  };
+  const handleUsernameChange = (e) => {
+    const newValue = e.target.value;
+    // Vérifie si la valeur contient uniquement des lettres (et autorise les espaces)
+    if (/^[a-zA-Z\s]*$/.test(newValue)) {
+      setUsername(newValue);
+    }
+  };
+  // Fonction pour valider le formulaire
+  const validateForm = () => {
+    const isEmailValid = email.match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\\.,;:\s@"]+\.)+[^<>()[\]\\.,;:\s@"]{2,})$/i
+    );
+    const isPasswordStrong = password.length > 8;
+    // Ajoutez d'autres règles selon le besoin
+
+    if (!isEmailValid) {
+      toast.error('Veuillez entrer une adresse email valide.');
+      return false;
+    }
+
+    if (!isPasswordStrong) {
+      toast.error('Le mot de passe doit contenir au moins 8 caractères.');
+      return false;
+    }
+
+    // Validez d'autres champs si nécessaire...
+
+    return true;
+  };
   // Fonction pour gérer la soumission du formulaire
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!validateForm()) {
+      return; // Arrête la soumission si la validation échoue
+    }
     try {
       // Faire une requête POST à l'API pour créer un nouvel utilisateur
       await api.post('/user/create', {
@@ -69,7 +143,7 @@ function Registration() {
               placeholder="Username *"
               required
               value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              onChange={handleUsernameChange}
             />
             <span className="icon user-icon" />
           </div>
@@ -93,9 +167,28 @@ function Registration() {
               placeholder="Mot de passe *"
               required
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handlePasswordChange}
             />
+            <div />
             <span className="icon lock-icon" />
+
+            {password && ( // N'affichez la barre et le label que si le mot de passe est non vide
+              <>
+                <div
+                  className={`password-strength-bar ${(() => {
+                    switch (passwordStrength.label) {
+                      case 'Fort':
+                        return 'strong';
+                      case 'Moyen':
+                        return 'medium';
+                      default:
+                        return 'weak';
+                    }
+                  })()}`}
+                />
+                <span>{passwordStrength.label}</span>
+              </>
+            )}
           </div>
           <button type="submit" className="register-button">
             Je m’inscris
